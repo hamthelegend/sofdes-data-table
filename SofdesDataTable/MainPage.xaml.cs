a34a35356d4478298ccb91a048c4cd227c3404dd
hamthelegend <hamuelxd@gmail.com>
2022-05-04 19:12:03 +0800

    Final (I hope) commit

--- SofdesDataTable/MainPage.xaml.cs
+++ SofdesDataTable/MainPage.xaml.cs
@@ -4,109 +4,122 @@ using Windows.UI.Xaml.Controls;
 
 namespace SofdesDataTable
 {
-
     public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
-
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
-        static readonly Phone[] samsungPhones =
+        public readonly PhoneManufacturer[] Manufacturers =
         {
-            new Phone("Galaxy S22", "Android", "Exynos 2200", 8),
-            new Phone("Galaxy Fold 3", "Android", "Snapdragon 888 5G", 12)
+            new PhoneManufacturer(
+                "Samsung",
+                "South Korea",
+                27.94,
+                "None",
+                "Not traded publicly",
+                new[]
+                {
+                    new Phone("Galaxy Z Fold3 5G", "Android 11", "Snapdragon 888 5G", 12),
+                    new Phone("Galaxy S22 Ultra 5G", "Android 12", "Exynos 2200", 12),
+                    new Phone("Galaxy S22+ 5G", "Android 12", "Exynos 2200", 8),
+                    new Phone("Galaxy S22 5G", "Android 12", "Exynos 2200", 8)
+                }
+            ),
+            new PhoneManufacturer(
+                "Apple",
+                "US",
+                27.68,
+                "None",
+                "AAPL",
+                new[]
+                {
+                    new Phone("iPhone 13 Pro Max", "iOS 15", "A15 Bionic", 6),
+                    new Phone("iPhone 13 Pro", "iOS 15", "A15 Bionic", 6),
+                    new Phone("iPhone 13", "iOS 15", "A15 Bionic", 4),
+                    new Phone("iPhone 13 Mini", "iOS 15", "A15 Bionic", 4)
+                }
+            ),
+            new PhoneManufacturer(
+                "Xiaomi",
+                "China",
+                12.18,
+                "None",
+                "SEHK: 1810",
+                new[]
+                {
+                    new Phone("12 Pro", "Android 12", "Snapdragon 8 Gen 1", 12),
+                    new Phone("12", "Android 12", "Snapdragon 8 Gen 1", 12),
+                    new Phone("Poco F4 GT", "Android 12", "Snapdragon 8 Gen 1", 12),
+                    new Phone("Poco M4 5G", "Android 12", "Dimensity 700", 6)
+                }
+            ),
+            new PhoneManufacturer(
+                "Huawei",
+                "China",
+                6.18,
+                "Huawei Investment & Holding",
+                "Not traded publicly",
+                new[]
+                {
+                    new Phone("Mate Xs 2", "HarmonyOS 2.0", "Snapdragon 888 4G", 12),
+                    new Phone("nova 9 SE 5G", "Android 11", "Snapdragon 695 5G", 8),
+                    new Phone("nova 9 SE", "Android 11", "Snapdragon 680 4G", 8),
+                    new Phone("nova Y70 Plus", "Android 11", "Unspecified", 4)
+                }
+            ),
+            new PhoneManufacturer(
+                "OPPO",
+                "China",
+                5.37,
+                "BBK Electronics",
+                "Not traded publicly",
+                new[]
+                {
+                    new Phone("K10 Pro", "Android 12", "Snapdragon 888 5G", 12),
+                    new Phone("K10 5G", "Android 12", "Dimensity 8000 Max", 12),
+                    new Phone("F21 Pro 5G", "Android 12", "Snapdragon 695 5G", 8),
+                    new Phone("F21 Pro", "Android 12", "Snapdragon 680 4G", 8),
+                    new Phone("Reno7", "Android 12", "Snapdragon 680 4G", 8)
+                }
+            )
         };
 
-        static readonly Phone[] applePhones =
-        {
-            new Phone("iPhone 13 Pro Max", "iOS", "A15 Bionic", 6),
-            new Phone("iPhone 13 Mini", "iOS", "A15 Bionic", 4)
-        };
+        private PhoneManufacturer _selectedManufacturer;
 
-        public PhoneManufacturer[] Manufacturers =
+        public PhoneManufacturer SelectedManufacturer
         {
-            new PhoneManufacturer("Samsung", "South Korea", 27.94, "None", "SSNLF", samsungPhones),
-            new PhoneManufacturer("Apple", "US", 27.68, "None", "AAPL", applePhones)
-        };
-        bool manufacturerSelected = false;
-        public bool ManufacturerSelected
-        {
-            get { return manufacturerSelected; }
-            set { 
-                manufacturerSelected = value;
-                OnPropertyChanged();
-            }
-        }
-        PhoneManufacturer selectedManufacturer = null;
-        public PhoneManufacturer SelectedManufacturer { 
-            get { return selectedManufacturer; } 
-            set {
-                selectedManufacturer = value;
-                OnPropertyChanged();
-            } 
-        }
-
-        int selectedManufacturerIndex = -1;
-        public int SelectedManufacturerIndex
-        {
-            get { return selectedManufacturerIndex; }
-            set {
-                ManufacturerSelected = value >= 0;
-                if (ManufacturerSelected)
+            get => _selectedManufacturer;
+            set
+            {
+                if (value != _selectedManufacturer)
                 {
-                    SelectedManufacturer = Manufacturers[value];
-                } else
-                {
-                    SelectedManufacturer = null;
+                    _selectedManufacturer = value;
+                    OnPropertyChanged();
                 }
-                selectedManufacturerIndex = value;
             }
         }
 
-        bool phoneSelected = false;
-        public bool PhoneSelected
+        private Phone _selectedPhone;
+
+        public Phone SelectedPhone
         {
-            get { return phoneSelected; }
-            set { 
-                phoneSelected = value;
-                OnPropertyChanged();
+            get => _selectedPhone;
+            set
+            {
+                if (value != _selectedPhone)
+                {
+                    _selectedPhone = value;
+                    OnPropertyChanged();
+                }
             }
         }
-        Phone selectedPhone = null;
-        public Phone SelectedPhone { 
-            get { return selectedPhone; } 
-            set {
-                selectedPhone = value;
-                OnPropertyChanged();
-            } 
-        }
-
-        int selectedPhoneIndex = -1;
-        public int SelectedPhoneIndex
-        {
-            get { return selectedPhoneIndex; }
-            set {
-                PhoneSelected = value >= 0;
-                if (PhoneSelected)
-                {
-                    SelectedPhone = SelectedManufacturer.Phones[value];
-                }
-                else
-                {
-                    SelectedPhone = null;
-                }
-                selectedPhoneIndex = value;
-            }
-        }
-
 
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
---
+++ SofdesDataTable/NullEnabledConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SofdesDataTable
+{
+    public class NullEnabledConverter: IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType,
+            object parameter, string language)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
